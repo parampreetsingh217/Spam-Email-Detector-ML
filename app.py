1049020f6c8ed0dbe237f83ea8f4c34315b67807
parampreetsingh217 <parampreetsingh787@gmail.com>
2024-08-01 20:44:49 +0400

    ML

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask , render_template, request
+import pickle
 
+app = Flask(__name__)
+cv = pickle.load(open("models/cv.pkl",'rb'))
+clf = pickle.load(open("models/clf.pkl",'rb'))
 
 @app.route("/")
 def home():
-    return "hello world update 217 " 
-
+    return render_template('index.html')
+ 
+@app.route("/predict", methods=["POST"])
+def predict():
+    email = request.form.get('content')
+    tokenized_email = cv.transform([email]) # X 
+    prediction = clf.predict(tokenized_email)
+    prediction = 1 if prediction == 1 else -1
+    return render_template("index.html", prediction=prediction, email=email) 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',debug=True)
